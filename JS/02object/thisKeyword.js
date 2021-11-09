80888e59ff440b89105acc66ba32886d8afffe08
deepakkumar1997 <pandeydeepak.kumar1997@gmail.com>
2021-11-09 22:19:07 +0530

    trying to understand this keyword in js

---
+++ JS/02object/thisKeyword.js
@@ -0,0 +1,31 @@
+// For all regular function calls, this keyword points to window object that is global object
+
+const friend = {
+    Name: "Anjan",
+    category: "PG: MCA",
+    Occupation: "Dev at TCS, Skecher",
+    sports: "Not so interested",
+    mode: "Disturbed",
+    database: "Filled with heartbreaks and computer science",
+    setMode: function(mode){
+        this.mode = mode;
+        function zone(){
+            console.log("Skeching");
+        }
+        zone();
+    }
+}
+console.log(friend.mode);
+// this is not regular function call this function is going to called from friend object
+friend.setMode("Stable");// But inside setMode() there is regular function call that is zone() so it
+// will be a function of global object window just like given below
+console.log(friend.mode);
+
+function sayFriendName(name){
+    console.log(name);
+}
+sayFriendName("Rishav");// regular function call and it is a method of window object
+
+/* for(const n in friend){
+    console.log(`Key: ${n} and value ${friend[n]}`);
+} */
